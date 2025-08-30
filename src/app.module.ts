a2322939c61706e98f6a01e4907ef6f1629223c3
gabriel-ganji <gabrielganji01011010@gmail.com>
2025-08-30 01:10:46 -0300

    feat: creating a database and acessing user routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,34 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import globalConfig from './global-config/global.config';
+import { GlobalConfigModule } from './global-config/global-config.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forFeature(globalConfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forFeature(globalConfig)],
+      inject: [globalConfig.KEY],
+      useFactory: async (globalConfigurations: ConfigType<typeof globalConfig>) => {
+        return {
+          type: globalConfigurations.database.type,
+          host: globalConfigurations.database.host,
+          port: globalConfigurations.database.port,
+          username: globalConfigurations.database.username,
+          database: globalConfigurations.database.database,
+          password: globalConfigurations.database.password,
+          autoLoadEntities: globalConfigurations.database.autoLoadEntities,
+          synchronize: globalConfigurations.database.synchronize,
+        }
+      }
+    }),
+    AuthModule, 
+    UserModule,
+    GlobalConfigModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+
+    async create(createUserDto: CreateUserDto) {
+        console.log("User: ", createUserDto);
+        return 'A user will be create in this service';
+    }
+
+}
